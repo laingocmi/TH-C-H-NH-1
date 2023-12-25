d877fd56d4e1e8ddc50b96238f3e94e060e4e735
miiii <154825977+laingocmi@users.noreply.github.com>
2023-12-25 23:25:55 +0700

    Add files via upload

---
+++ # lession 1.py
@@ -0,0 +1,15 @@
+# BT1
+print("hello world")
+# BT 2
+a = "HELLO WORLD"
+print(a)
+# BT3
+b = "hello"
+c = "world"
+d = " "
+print(b+d+c)
+# BT 4
+def print_helloworld():
+    print("HELLO WORLD")
+    return ;
+print_helloworld()
